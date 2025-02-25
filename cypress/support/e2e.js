4f1dedba0ee66dee16ea1ca190b48df22905d8f4
Leandro S Castro <leandro2509@gmail.com>
2025-02-25 17:45:37 -0300

    Add API validation tests for student routes and corresponding commands

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -6,6 +6,7 @@ import './commands_api/commands_alimentos'
 import './commands_api/commands_alergias_intolerancias'
 import './commands_api/commands_alimentos_da_guia'
 import './commands_api/commands_alteracoes_cardapio'
+import './commands_api/commands_alunos'
 
 import './commands_ui/commands_login'
 import './commands_ui/commands_globais'
