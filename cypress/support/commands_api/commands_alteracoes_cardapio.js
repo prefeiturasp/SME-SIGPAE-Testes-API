10e0a2cd9c51a7d680aa493ec4751742a80ebb95
Leandro S Castro <leandro2509@gmail.com>
2025-03-20 14:44:24 -0300

    Remoção do endpoint excluído no sistema

--- cypress/support/commands_api/commands_alteracoes_cardapio.js
+++ cypress/support/commands_api/commands_alteracoes_cardapio.js
@@ -23,21 +23,12 @@ Cypress.Commands.add('validar_alteracoes_cardapio_relatorio', (id) => {
 	})
 })
 
-Cypress.Commands.add('validar_alteracoes_cardapio_com_lanche_do_mes_corrente', (id) => {
-	cy.request({
-		method: 'GET',
-		url: Cypress.config('baseUrl') + `api/alteracoes-cardapio/com-lanche-do-mes-corrente/${id}`,
-		headers: {
-			Authorization: 'JWT ' + globalThis.token,
-		},
-		failOnStatusCode: false,
-	})
-})
-
 Cypress.Commands.add('validar_alteracoes_cardapio_minhas_solicitacoes', () => {
 	cy.request({
 		method: 'GET',
-		url: Cypress.config('baseUrl') + 'api/alteracoes-cardapio/minhas-solicitacoes/',
+		url:
+			Cypress.config('baseUrl') +
+			'api/alteracoes-cardapio/minhas-solicitacoes/',
 		headers: {
 			Authorization: 'JWT ' + globalThis.token,
 		},
@@ -58,15 +49,11 @@ Cypress.Commands.add('cadastrar_alteracoes_cardapio', (dados_teste) => {
 			substituicoes: [
 				{
 					periodo_escolar: dados_teste.periodo_escolar,
-					tipos_alimentacao_de: [
-						dados_teste.tipos_alimentacao_de
-					],
+					tipos_alimentacao_de: [dados_teste.tipos_alimentacao_de],
 					alteracao_cardapio: dados_teste.alteracao_cardapio,
-					tipos_alimentacao_para: [
-						dados_teste.tipos_alimentacao_para
-					],
-					qtd_alunos: dados_teste.qtd_alunos
-				}
+					tipos_alimentacao_para: [dados_teste.tipos_alimentacao_para],
+					qtd_alunos: dados_teste.qtd_alunos,
+				},
 			],
 			datas_intervalo: [
 				{
@@ -75,16 +62,17 @@ Cypress.Commands.add('cadastrar_alteracoes_cardapio', (dados_teste) => {
 					cancelado: dados_teste.cancelado,
 					cancelado_justificativa: dados_teste.cancelado_justificativa,
 					cancelado_em: dados_teste.cancelado_em,
-					cancelado_por: dados_teste.cancelado_por
-				}
+					cancelado_por: dados_teste.cancelado_por,
+				},
 			],
 			data_inicial: dados_teste.data,
 			data_final: dados_teste.data,
 			observacao: dados_teste.observacao,
 			foi_solicitado_fora_do_prazo: dados_teste.foi_solicitado_fora_do_prazo,
 			terceirizada_conferiu_gestao: dados_teste.terceirizada_conferiu_gestao,
-			eh_alteracao_com_lanche_repetida: dados_teste.eh_alteracao_com_lanche_repetida,
-			criado_por: dados_teste.criado_por
+			eh_alteracao_com_lanche_repetida:
+				dados_teste.eh_alteracao_com_lanche_repetida,
+			criado_por: dados_teste.criado_por,
 		},
 		failOnStatusCode: false,
 	})
