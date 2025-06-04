88195b18697ce2ccad8f158ff16aad22d21a4e83
Leandro S Castro <leandro2509@gmail.com>
2025-06-04 17:28:47 -0300

    Add new Cypress commands for validating cronogramas data and dashboard

--- cypress/support/commands_api/commands_cronogramas.js
+++ cypress/support/commands_api/commands_cronogramas.js
@@ -11,3 +11,56 @@ Cypress.Commands.add('validar_cronogramas', (parametros) => {
 		failOnStatusCode: false,
 	})
 })
+
+Cypress.Commands.add('validar_dados_cronograma_ficha_recebimento', (uuid) => {
+	cy.request({
+		method: 'GET',
+		url:
+			Cypress.config('baseUrl') +
+			`api/cronogramas/${uuid}/dados-cronograma-ficha-recebimento/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('validar_detalhar_com_log', (uuid) => {
+	cy.request({
+		method: 'GET',
+		url:
+			Cypress.config('baseUrl') + `api/cronogramas/${uuid}/detalhar-com-log/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('validar_dashboard', (uuid) => {
+	cy.request({
+		method: 'GET',
+		url: Cypress.config('baseUrl') + 'api/cronogramas/dashboard/',
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('validar_dashboard_com_filtro', (filtro) => {
+	cy.request({
+		method: 'GET',
+		url:
+			Cypress.config('baseUrl') +
+			`api/cronogramas/dashboard-com-filtro/${filtro}`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
