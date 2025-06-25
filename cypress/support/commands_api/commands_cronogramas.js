3a2eb9c2afd0213174240ae11f999fc11e2df266
Leandro S Castro <leandro2509@gmail.com>
2025-06-25 17:11:03 -0300

    Add API commands for creating and deleting cronogramas with validation tests

--- cypress/support/commands_api/commands_cronogramas.js
+++ cypress/support/commands_api/commands_cronogramas.js
@@ -127,3 +127,59 @@ Cypress.Commands.add('validar_rascunhos', () => {
 		failOnStatusCode: false,
 	})
 })
+
+Cypress.Commands.add('cadastrar_cronogramas', (dados_teste) => {
+	cy.request({
+		method: 'POST',
+		url: Cypress.config('baseUrl') + 'api/cronogramas/',
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		body: {
+			armazem: dados_teste.armazem,
+			empresa: dados_teste.empresa,
+			contrato: dados_teste.contrato,
+			unidade_medida: dados_teste.unidade_medida,
+			qtd_total_programada: dados_teste.qtd_total_programada,
+			etapas: [
+				{
+					numero_empenho: dados_teste.etapas.numero_empenho,
+					etapa: dados_teste.etapas.etapa,
+					parte: dados_teste.etapas.parte,
+					data_programada: dados_teste.etapas.data_programada,
+					quantidade: dados_teste.etapas.quantidade,
+					total_embalagens: dados_teste.etapas.total_embalagens,
+					qtd_total_empenho: dados_teste.etapas.qtd_total_empenho,
+					uuid: dados_teste.etapas.uuid,
+				},
+			],
+			programacoes_de_recebimento: [
+				{
+					data_programada:
+						dados_teste.programacoes_de_recebimento.data_programada,
+					tipo_carga: dados_teste.programacoes_de_recebimento.tipo_carga,
+					uuid: dados_teste.programacoes_de_recebimento.uuid,
+				},
+			],
+			ficha_tecnica: dados_teste.ficha_tecnica,
+			tipo_embalagem_secundaria: dados_teste.tipo_embalagem_secundaria,
+			custo_unitario_produto: dados_teste.custo_unitario_produto,
+			uuid: dados_teste.uuid,
+			observacoes: dados_teste.observacoes,
+		},
+		failOnStatusCode: false,
+	})
+})
+
+Cypress.Commands.add('deletar_cronograma', (uuid) => {
+	cy.request({
+		method: 'DELETE',
+		url: Cypress.config('baseUrl') + `api/cronogramas/${uuid}/`,
+		timeout: 60000,
+		headers: {
+			Authorization: 'JWT ' + globalThis.token,
+		},
+		failOnStatusCode: false,
+	})
+})
